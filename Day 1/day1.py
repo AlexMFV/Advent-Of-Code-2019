c6b209b749e580b2c6b3241debb58b46f5ef5d99
AlexMFV <alexmfv1@gmail.com>
2020-02-06 23:16:01 +0000

    Day 1 Part 2

--- Day 1/day1.py
+++ Day 1/day1.py
@@ -2,15 +2,26 @@ from math import *
 
 def main():
     file = open("input.txt", "r")
+    output = open("output.txt", "w")
     sum = 0
+    sum2 = 0
 
     for line in file.readlines():
-        sum += processLine(line);
+        isZero = False
+        value = int(line)
+        sum = calculate(value)
 
-    output = open("output.txt", "w")
-    output.write(str(sum))
+        while isZero is False:
+            if calculate(value) > 0:
+                sum2 += calculate(value);
+                value = calculate(value)
+            else:
+                isZero = True
 
-def processLine(line):
-    return floor(int(line) / 3) - 2
+    output.write(str(sum)+ "\n")
+    output.write(str(sum2))
+
+def calculate(value):
+    return floor(value / 3) - 2
 
 main()
